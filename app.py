d5a3fa94322509877a2c9db9ab61359819970995
Bhargab jyoti Bhuyan <bjbcr7@gmail.com>
2026-01-17 00:08:09 +0530

    model training and hyperparametr tuning

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from src.DataScienceProject.exception import CustomException,sys
 from src.DataScienceProject.components.data_ingestion import DataIngestion
 from src.DataScienceProject.components.data_ingestion import DataIngestionConfig
 from src.DataScienceProject.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.DataScienceProject.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 if __name__ =="__main__":   
   logging.info("The execution has started")
@@ -14,7 +15,10 @@ if __name__ =="__main__":
     train_data_path, test_data_path=data_ingestion.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+    train_arr, test_arr,_= data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+    
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
     
   except Exception as e:
     logging.info('Custom Exception')
