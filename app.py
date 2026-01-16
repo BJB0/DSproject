9e79eb465b70598f17adf6fa8fae237835346849
Bhargab jyoti Bhuyan <bjbcr7@gmail.com>
2026-01-16 19:32:19 +0530

    data transformation

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.DataScienceProject.logger import logging
 from src.DataScienceProject.exception import CustomException,sys
 from src.DataScienceProject.components.data_ingestion import DataIngestion
 from src.DataScienceProject.components.data_ingestion import DataIngestionConfig
+from src.DataScienceProject.components.data_transformation import DataTransformationConfig, DataTransformation
 
 if __name__ =="__main__":   
   logging.info("The execution has started")
@@ -10,7 +11,11 @@ if __name__ =="__main__":
   
   try:
     data_ingestion=DataIngestion()
-    data_ingestion.initiate_data_ingestion()
+    train_data_path, test_data_path=data_ingestion.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+    
   except Exception as e:
     logging.info('Custom Exception')
     raise CustomException(e,sys)
